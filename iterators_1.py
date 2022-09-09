e207a46fb65263c9b3402845d82a8ff3fe958f4a
PankratovEP <enkwi@mail.ru>
2022-09-09 23:37:55 +0300

    Написал собственные классы итераторов

--- iterators_1.py
+++ iterators_1.py
@@ -1,10 +1,11 @@
-lst = [1, True, 0, 'ddfg', 12.222, [1,2]]
+from random import *
+'''lst = [1, True, 0, 'ddfg', 12.222, [1,2]]
 iterator = iter(lst)
-'''print(next(iterator))
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
-print(next(iterator))'''
+print(next(iterator))
+print(next(iterator))
 my_iter = iterator
 while True:
     try:
@@ -12,3 +13,38 @@ while True:
         print(i)
     except StopIteration:
         break
+        '''
+class RandomIterator:
+    def __iter__(self):
+        return self
+
+    def  __init__(self, k):
+        self.k = k
+        self.i = 0
+
+    def __next__(self):
+        if self.i < self.k:
+            self.i += 1
+        else: raise StopIteration
+        return random()
+
+for x in RandomIterator(6):
+    print(x)
+
+class  DoubleElementListIterator:
+    def __init__(self, lst):
+        self.lst = lst
+        self.i = 0
+    def __next__(self):
+        if self.i < len(self.lst):
+            self.i += 2
+            return self.lst[self.i - 2], self.lst[self.i - 1]
+        else:  raise StopIteration
+class MyList(list):
+    def __iter__(self):
+        return DoubleElementListIterator(self)
+for pair in MyList([1,23,7,8]):
+    print(pair)
+
+
+
