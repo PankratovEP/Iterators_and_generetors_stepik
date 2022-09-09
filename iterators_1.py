03e82c35492ac7d0a4f6d3814061b3ec84ddd65d
PankratovEP <enkwi@mail.ru>
2022-09-09 22:17:35 +0300

    написал собственный итератор

--- iterators_1.py
+++ iterators_1.py
@@ -1,8 +1,14 @@
-lst = [1, True, 0, 'ddfg', 12.222]
+lst = [1, True, 0, 'ddfg', 12.222, [1,2]]
 iterator = iter(lst)
 '''print(next(iterator))
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))'''
-
+my_iter = iterator
+while True:
+    try:
+        i = next(my_iter)
+        print(i)
+    except StopIteration:
+        break
